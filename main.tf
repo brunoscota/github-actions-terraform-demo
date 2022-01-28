ed1e7e4aa1bbea449c0d7d33809ba765b3d15ed2
Bruno Scota de Carvalho <brunoscota@gmail.com>
2022-01-27 22:34:34 -0300

    first commit

--- main.tf
+++ main.tf
@@ -7,6 +7,15 @@ resource "random_string" "random" {
   override_special = "/@£$"
 }
 
+terraform {
+  backend "azurerm" {
+    storage_account_name = "daczxcase3asd"
+    container_name       = "state"
+    key                  = "terraform.tfstate"
+    sas_token            = "?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-01-28T09:33:37Z&st=2022-01-28T01:33:37Z&spr=https&sig=UHhmmqAVsEtQokf7IhKK591GeDbp%2F8vz%2F0%2F1Boqd9eg%3D"
+  }
+}
+
 output "random_string" {
   value = random_string.random.id
 }
