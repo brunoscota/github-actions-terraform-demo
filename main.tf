827a944b99afa3af87145917264d0fcdaa33e388
Bruno Scota de Carvalho <brunoscota@gmail.com>
2022-01-27 22:08:01 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "random" {
+}
+
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
+
+output "random_string" {
+  value = random_string.random.id
+}
